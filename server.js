d0f15770567e89fd869bc588d2b44bf40eedef41
Tate Lewis <tatelewis0@gmail.com>
2024-02-13 19:13:14 -0500

    Added default and updated index.html

--- server.js
+++ server.js
@@ -9,11 +9,19 @@ const server = http.createServer((req, res) => {
     
     switch(parsedUrl.pathname){
         case '/':
-            res.writeHead(200, {
-                'Content-Type' : 'text/plain'
-            })
-            res.write('Hey')
-            res.end('Hello, welcome to the store!')
+            if(req.method==='GET') {
+                res.writeHead(200, {
+                    'Content-Type' : 'text/html'
+                })
+                fs.readFile('index.html', (err, file) => {
+                    if(err) {
+                        res.end('Internal server error!')
+                    } else {
+                        res.end(file)
+                    }
+                })
+
+            }
             console.log(`Path accessed = ${req.url}`)
         break;
         case '/home':
